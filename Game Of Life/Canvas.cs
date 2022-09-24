95fcabc77a1389469497e101573fd62384fac67a
Austin Hamilton <HAMILTONAJ@ETSU.EDU>
2022-09-24 18:38:20 -0400

    Add project files.

---
+++ Game Of Life/Canvas.cs
@@ -0,0 +1,15 @@
+namespace Game_Of_Life
+{
+    public partial class Canvas : Form
+    {
+        public Canvas()
+        {
+            InitializeComponent();
+        }
+
+        public void Init()
+        {
+
+        }
+    }
+}
